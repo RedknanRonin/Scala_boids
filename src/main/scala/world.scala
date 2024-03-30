35ff835f6b479ae0262c0140be746c50932000a9
aleks_3vs4n4y <aleksi.alatalo@gmail.com>
2024-03-30 20:59:59 +0200

    boids are actual triangles, starting evolution

--- src/main/scala/world.scala
+++ src/main/scala/world.scala
@@ -1,5 +1,6 @@
 package main
 
+import scala.collection.mutable.ArrayBuffer
 import scala.util.Random
 
 class world {
@@ -7,22 +8,39 @@ class world {
   val windowWidth= 740
   var mutationChance:Double=0
   val seed= Random(69420)
-  var listOfBoids=Array[Boid]()
+  var listOfBoids=ArrayBuffer[Boid]()
+  var listOfFoods=ArrayBuffer[Food]()
+  
+  var simulationWorldEnabled=true
 
+  var foodTimer=0
+  var foodSpawnInterval = 150
 
+  def toggleSimulation=simulationWorldEnabled= !simulationWorldEnabled
   def setMutationChance(v:Double) = mutationChance=v
   def spawnBoid (b:Boid) = listOfBoids=listOfBoids.appended(b)
   def getBoid(number:Int) =  listOfBoids(number)
   def printBoids = for i <- listOfBoids.indices do println(i +" "+listOfBoids(i))
-
   def amountOfBoids = listOfBoids.length
+  def setFoodInterval(withVal:Int) = foodSpawnInterval=withVal
 
   def getBoidAt(at:Point) =
     val boidsWithPos:Map[Point,Boid]=listOfBoids.map(a=> a.pos -> a).toMap  // returns a boid at a given point
     boidsWithPos.lift(at).get
 
-  def deleteBoid(at:Point) = listOfBoids.filter(a => a!=getBoidAt(at))
+  def deleteBoid(b:Boid) = listOfBoids-=b
+  def deleteFood(food: Food) = listOfFoods-=food
 
-  def tick= for each<- listOfBoids do each.move()
+  def tick=
+    for each<- listOfBoids do each.move()
+
+  def updateFoodTimer=
+    if foodTimer == foodSpawnInterval then
+      spawnFood(Food(Point(seed.between(0, windowWidth), seed.between(0, windowHeight)),this))
+      foodTimer = 0
+    else foodTimer += 1
+
+  def spawnFood(f:Food) =
+    listOfFoods= listOfFoods.appended(f)
 
 }
